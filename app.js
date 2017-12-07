0d6b41e318e8f768318a3eda39be02772c1ac8bd
Kardaev <mdkardaev@chel.rucard.ru>
2017-12-07 11:44:24 +0300

    3

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var keyPair = bitcoin.ECPair.fromWIF('cV5bupsrQH59Nh9QGgzyk8b6UtGKQRtr1Ekrye2GRF
 
 var tx = new bitcoin.TransactionBuilder(testnet);
 tx.addInput("c24b65aa607d5f39e3ab77deac30a9adbf8f8dddaaf61af34f8304f1ad183a20", 0);
-tx.addOutput("mgRoeWs2CeCEuqQmNfhJjnpX8YvtPACmCX", 130000000);
+tx.addOutput("2N8hwP1WmJrFF5QWABn38y63uYLhnJYJYTF", 130000000);
 tx.sign(0, keyPair);
 
 let txHash = tx.build().toHex()
@@ -27,3 +27,5 @@ console.log(txHash)
 
 
 // 0100000001203a18adf104834ff31af6aadd8d8fbfada930acde77abe3395f7d60aa654bc2000000006a47304402206849cc43e753778b5eaf8e9067dfb8156491fc643463e85ce1fc519e23aa785f022061943b579fcf24913f0e7ac662817014aadb145af77e5f35003bdbab31076d4c01210243f77b392dcbe1572d7f19af8ef191a554d5a5e1e0ace23b3ba64211809f3939ffffffff0180a4bf07000000001976a91409fed3e08e624b23dbbacc77f7b2a39998351a6888ac00000000
+
+//f7dd525b5b2d2d34788a674c077f62796d04ce8ba7420761b4ab60e7c53e22e8
