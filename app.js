31bbff145297ef111df7c2e4c00a03455b2f3be8
Mikhail <kardaev.74@gmail.com>
2017-12-10 21:47:36 +0500

    4

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var bitcoin = require("bitcoinjs-lib")
 var axios = require("axios")
+var bip39 = require("bip39")
 
 var testnet = bitcoin.networks.testnet
 
@@ -8,24 +9,55 @@ var testnet = bitcoin.networks.testnet
 // console.log(keyPair.getAddress());
 // console.log(keyPair.toWIF());
 
-// msfjpY6BfACW5Rj5wpdJi9VXgKRD4swuC7
-// cV5bupsrQH59Nh9QGgzyk8b6UtGKQRtr1Ekrye2GRFFiW5xrMS6N
+// address: mjgMoPhHkYnWcJyTwQAasSUZjwYknWfbFf
+// wif: cVGi5WiBMLSGbWDsadZNdCzpm4zXJa8LHAm8p5ytbS8VdQB9Bj3e
+
+//faucet tx: 945486ada7998934fdecbbd6dc9ee6e0117a57ba3d766c1875c9edbcaea2c173
 
 //load from priveKey WIF
-var keyPair = bitcoin.ECPair.fromWIF('cV5bupsrQH59Nh9QGgzyk8b6UtGKQRtr1Ekrye2GRFFiW5xrMS6N', testnet)
+// var keyPair = bitcoin.ECPair.fromWIF('cVGi5WiBMLSGbWDsadZNdCzpm4zXJa8LHAm8p5ytbS8VdQB9Bj3e', testnet)
+//
+// var tx = new bitcoin.TransactionBuilder(testnet);
+// tx.addInput("945486ada7998934fdecbbd6dc9ee6e0117a57ba3d766c1875c9edbcaea2c173", 0);
+// tx.addOutput("2N8hwP1WmJrFF5QWABn38y63uYLhnJYJYTF", 129000000);
+// tx.sign(0, keyPair);
+//
+// let txHash = tx.build().toHex()
+// console.log(txHash)
 
-// console.log(keyPair.getAddress());
-// console.log(keyPair.toWIF());
-
-var tx = new bitcoin.TransactionBuilder(testnet);
-tx.addInput("c24b65aa607d5f39e3ab77deac30a9adbf8f8dddaaf61af34f8304f1ad183a20", 0);
-tx.addOutput("2N8hwP1WmJrFF5QWABn38y63uYLhnJYJYTF", 130000000);
-tx.sign(0, keyPair);
-
-let txHash = tx.build().toHex()
-console.log(txHash)
+//0107d8ecbd068984472238b3be982ad2d54b5cc9e8705ead6c4a14bf824f9464
 
 
-// 0100000001203a18adf104834ff31af6aadd8d8fbfada930acde77abe3395f7d60aa654bc2000000006a47304402206849cc43e753778b5eaf8e9067dfb8156491fc643463e85ce1fc519e23aa785f022061943b579fcf24913f0e7ac662817014aadb145af77e5f35003bdbab31076d4c01210243f77b392dcbe1572d7f19af8ef191a554d5a5e1e0ace23b3ba64211809f3939ffffffff0180a4bf07000000001976a91409fed3e08e624b23dbbacc77f7b2a39998351a6888ac00000000
+//MNEMONIC
+// let mnemonic = bip39.generateMnemonic()
+// var seed = bip39.mnemonicToSeed(mnemonic)
+//
+// var hdMaster = bitcoin.HDNode.fromSeedBuffer(seed, testnet)
+//
+// console.log(hdMaster.toBase58())
+// // tprv8ZgxMBicQKsPdyFHnaAgCK6sG7AYVmmg1L95QYkxXX7huKs2sj1zetgJMZDT9WxAwzvX5wRyzYzdr416f8Bmdypx8P8qrXcLuhmW7Au2D6e
+//
+// var key1 = hdMaster.derivePath('m/0')
+// var key2 = hdMaster.derivePath('m/1')
+// // "m/44'/0'/0'/0/0"
+// console.log(key1.keyPair.getAddress())
+// console.log(key2.keyPair.getAddress())
+// // key1 address: mtWa7DKPNwfVQ9ivgb6XUonim5gfCJv17p
+// // key2 address: mrL1bP556Q19Q1WWcjYEFdvhsnLu3S4W5p
 
-//f7dd525b5b2d2d34788a674c077f62796d04ce8ba7420761b4ab60e7c53e22e8
+
+
+// Restore
+// var hdMaster = bitcoin.HDNode.fromBase58('tprv8ZgxMBicQKsPdyFHnaAgCK6sG7AYVmmg1L95QYkxXX7huKs2sj1zetgJMZDT9WxAwzvX5wRyzYzdr416f8Bmdypx8P8qrXcLuhmW7Au2D6e', testnet)
+// var keyPair = hdMaster.derivePath('m/0').keyPair
+// console.log(keyPair.getAddress())
+//
+// var tx = new bitcoin.TransactionBuilder(testnet);
+// tx.addInput("dec298af232404e42c3d20c770450fbdb7cd059b6945ff70f9e82befc8ee0739", 0);
+// tx.addOutput("2N8hwP1WmJrFF5QWABn38y63uYLhnJYJYTF", 03240000);
+// tx.sign(0, keyPair);
+//
+// let txHash = tx.build().toHex()
+// console.log(txHash)
+
+//01000000013907eec8ef2be8f970ff45699b05cdb7bd0f4570c7203d2ce4042423af98c2de000000006a473044022031f8209bd7e838381f88f2e577018f669c8c44b9d8b71e0479bb988f7179fc8e02202dfe5718eed0dd062bfdf9c759c06df6c0bf221c1e84697d01281bd8bb5d87a701210339027ee0b470f5bd270d128cf0121dd910f20a3161de5581276f145177c07e46ffffffff0100006a000000000017a914a9974100aeee974a20cda9a2f545704a0ab54fdc8700000000
