8971f3ede4cfa682ec188ecdef5abd4fe54321ce
Mihail74 <Mihail74@users.noreply.github.com>
2017-12-24 20:55:40 +0500

    add keystone (#1)

--- src/models/Resource.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Resource = new mongoose.Schema({
-  path: String,
-  price: Number, // in satoshi
-  content: String
-})
-
-module.exports = mongoose.model('Resource', Resource)
